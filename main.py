0b57627b02f1903b560a6da09ffae8cbeedcdf3e
dedeepyakona <dedeepyakona07@gmail.com>
2026-04-19 13:04:18 +0530

    Update database connection defaults in main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 # Database connection using Railway environment variables
 conn = mysql.connector.connect(
-    host=os.environ.get("MYSQLHOST"),
-    user=os.environ.get("MYSQLUSER"),
+    host=os.environ.get("MYSQLHOST", "mysql.railway.internal"),
+    user=os.environ.get("MYSQLUSER", "root"),
     password=os.environ.get("MYSQLPASSWORD"),
-    database=os.environ.get("MYSQLDATABASE"),
+    database=os.environ.get("MYSQLDATABASE", "railway"),
     port=int(os.environ.get("MYSQLPORT", 3306))
 )
 
