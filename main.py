77d5ffbe73e688fadf3b0bffccdb46d01a6ee184
dedeepyakona <dedeepyakona07@gmail.com>
2026-04-19 12:51:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ conn = mysql.connector.connect(
     user=os.environ.get("MYSQLUSER"),
     password=os.environ.get("MYSQLPASSWORD"),
     database=os.environ.get("MYSQLDATABASE"),
-    port=int(os.environ.get("MYSQLPORT" 3306))
+    port=int(os.environ.get("MYSQLPORT", 3306))
 )
 
 cursor = conn.cursor()
